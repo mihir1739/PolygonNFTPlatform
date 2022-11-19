569fd1c4c40752537b295e2b7e8b7b21433b187e
mihir1739 <mihir39nr@gmail.com>
2022-11-19 14:57:53 +0530

    Updated Frontend

---
+++ frontend/out/_next/static/quD9Gy9tYI7oMh3R0hXyu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/44-c6982862db4f83be.js","static/css/ae0e3e027412e072.css","static/chunks/pages/index-7d6b9ac825803985.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
